7592dbcf45fc770115a48290d47114d1194cb1a2
James Shin <jhscode@gmail.com>
2019-01-16 13:36:35 -0500

    installed particles, added to background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 100
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm />
-          {/* <FaceRecognition /> */}
+        <Particles 
+          className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
