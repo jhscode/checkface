f46a0a3f9489b412bfba080ce967b28b843d6480
James Shin <jhscode@gmail.com>
2019-01-16 12:24:08 -0500

    installed tachyons, added background, styled link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
   return (
     <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <p>Sign out</p>
+      <p className="f3 link dim black underline pa3 pointer">Sign out</p>
     </nav>
   );
 }
