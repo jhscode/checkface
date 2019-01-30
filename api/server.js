6263b35ae6fbc572535fe008d8872366c997cea3
James Shin <jhscode@gmail.com>
2019-01-30 07:30:24 -0500

    working on backend. added basics of server

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+module.export = app;
