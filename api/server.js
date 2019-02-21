7fbde46ace556b11f859bab33d94c69fd6ca4727
James Shin <jhscode@gmail.com>
2019-02-21 08:12:52 -0500

    added router, updated server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const signin = requre('./routes/signin');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use('/signin', signin);
+
 
 module.export = app;
