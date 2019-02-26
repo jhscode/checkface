a44d42fc90debe3024777664db0a54b4d0a0b184
James Shin <jhscode@gmail.com>
2019-02-26 08:26:42 -0500

    server up and running

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose')
+
+const server = require('./api/server');
+const { PORT, DB_URI } = require('./api/utils/constants');
+
+server.listen(PORT, async () => {
+  await mongoose.connect(DB_URI)
+  console.log(`Database connected at ${DB_URI}`)
+  console.log(`App listening on port ${PORT}`)
+});
